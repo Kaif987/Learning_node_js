158be843228b89026cef6c9a01f058df1ccd6f51
Kaif Siddiqui <mdkaifprofession@gmail.com>
2022-11-04 00:07:19 +0530

    Add Export file and Import in Index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require("http")
+const date = require("./DateTimeModule")
+
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html" })
-    res.end()
+    res.end(date.myDateTime())
 }).listen(8080)
