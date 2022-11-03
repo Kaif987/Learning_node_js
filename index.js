1f99c79ccc88303cbe7bff403847a7a45edebcab
Kaif Siddiqui <mdkaifprofession@gmail.com>
2022-11-04 00:24:00 +0530

    Read Index.html file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const http = require("http")
+const fs = require("fs")
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/html" })
-    res.write(req.url)
-    res.end()
+    fs.readFile("index.html", function(err, data){
+        res.writeHead(200, {"Content-Type" : "text/html"})
+        res.write(data)
+        return res.end()
+    })
 }).listen(8080)
